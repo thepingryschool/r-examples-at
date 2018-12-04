9c5ad449740ec576259172bd2f3d0d078d59b1df
Aye Thuzar <athuzar@pingry.org>
2018-12-04 10:03:43 -0500

    Day 13 Examples

--- webHarvesting2.R
+++ webHarvesting2.R
@@ -1,17 +1,17 @@
 library(rvest)
 library(dplyr)
 library(stringr)
-library(readr)
+library(tidyverse)
 
 # web table to dataframe
 url = "https://en.wikipedia.org/wiki/Murder_in_the_United_States_by_state"
 murders_raw = read_html(url) %>% html_nodes("table") %>% html_table()
 murders_raw = murders_raw[[2]] %>% setNames(c("state", "population", "total", "murders", "gun_murders", "gun_ownership", "total_rate", "murder_rate", "gun_murder_rate"))
-head(murders_raw)
+#head(murders_raw)
 str(murders_raw)
 
 
-# character with commas to numeric 
+# character with commas to numeric
 commas = function(x){ any(str_detect(x, ",")) }
 murders_raw %>% summarize_all(funs(commas))
 
