2d358d71b13413435515717fabe43c5e7df16292
Aye Thuzar <athuzar@pingry.org>
2018-12-11 11:48:22 -0500

    hackathon csv's

--- pdfHarvesting.R
+++ pdfHarvesting.R
@@ -3,7 +3,7 @@ temp_file <- tempfile()
 url <- "http://www.pnas.org/content/suppl/2015/09/16/1510159112.DCSupplemental/pnas.201510159SI.pdf"
 download.file(url, temp_file)
 txt <- pdf_text(temp_file)
-file.remove(temp_file)
+#file.remove(temp_file)
 
 raw_data_research_funding_rates <- txt[2]
 raw_data_research_funding_rates
